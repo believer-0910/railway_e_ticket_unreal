e3ae48ea0c2885a42aaf8123bf44d6f57c217a28
SaidaminQambarov <93591690+SaidaminQambarov@users.noreply.github.com>
2022-03-01 20:20:22 +0500

    Add files via upload

---
+++ railway_e_tickets_unreal/src/main/java/com/example/railway_e_tickets_unreal/RailwayETicketsUnrealApplication.java
@@ -0,0 +1,13 @@
+package com.example.railway_e_tickets_unreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayETicketsUnrealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayETicketsUnrealApplication.class, args);
+    }
+
+}
---
+++ railway_e_tickets_unreal/src/test/java/com/example/railway_e_tickets_unreal/RailwayETicketsUnrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railway_e_tickets_unreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayETicketsUnrealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
